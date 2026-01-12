631f5515b2f6b44d075ac037e6a5b484f2285e0a
shellRaining <shellRaining@gmail.com>
2026-01-12 14:29:31 +0800

    fix: na not show navidorome

--- lua/single_char_filter.lua
+++ lua/single_char_filter.lua
@@ -11,8 +11,8 @@ function M.func(input, env)
     local context = env.engine.context
     local input_code = context.input
 
-    -- 如果输入码长度为 1，则过滤掉 completion 类型的候选（英文补全）
-    if utf8.len(input_code) == 1 then
+    -- 如果输入码长度小于等于 2，则过滤掉 completion 类型的候选（英文补全）
+    if utf8.len(input_code) <= 2 then
         for cand in input:iter() do
             local cand_type = cand.type
             -- 过滤掉 completion 类型的候选项（这些是来自 custom_phrase 和 melt_eng 的前缀补全）
@@ -21,7 +21,7 @@ function M.func(input, env)
             end
         end
     else
-        -- 输入码长度大于 1 时，正常显示所有候选
+        -- 输入码长度大于 2 时，正常显示所有候选
         for cand in input:iter() do
             yield(cand)
         end
